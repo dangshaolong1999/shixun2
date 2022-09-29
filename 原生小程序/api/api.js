064042ce2b0d4498e51f3287b806aa6b6c821b20
dangshaolong1999 <3092245754@qq.com>
2022-09-29 21:37:55 +0800

    '小程序2'

--- 原生小程序/api/api.js
+++ 原生小程序/api/api.js
@@ -5,11 +5,34 @@ import request  from '../http/http' //引入上面封装好的请求方法
 //     url:'home/getFirstClassify'
 //   })
 // }
-export const getTopCate=(data = {})=> {
+//请求轮播
+ const getTopCate=(data = {})=> {
     return request({
         // baseURL: hostConfig.xxx,
         url: "app/home/getSecondClassify/1?classify_id=1",
         method: "GET",
         data,
     });
+}
+//请求导航
+const getbanner=(data = {})=> {
+    return request({
+        // baseURL: hostConfig.xxx,
+        url: "app/home/banner/1/4",
+        method: "GET",
+        data,
+    });
+}
+//请求免费直播课
+const getFree = (data = {}) => {
+    return request({
+        url: "app/home/marketingCourse/1?classify_id=1",
+        method: "GET",
+        data,
+    })
+}
+export {
+    getTopCate,
+    getbanner,
+    getFree
 }
--- 原生小程序/pages/index/index.js
+++ 原生小程序/pages/index/index.js
@@ -1,15 +1,17 @@
 // index.js
 // 获取应用实例
 const app = getApp()
-
+import { getTopCate, getbanner,getFree } from '../../api/api.js'
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    //导航
+    list: [],
+    //轮播
+    bannerList: [],
+    //免费课
+    freeList: [],
+    //讲师头像
+    avatarList: []
   },
   // 事件处理函数
   bindViewTap() {
@@ -18,11 +20,22 @@ Page({
     })
   },
   onLoad() {
-    if (wx.getUserProfile) {
+    getTopCate().then(res => {
       this.setData({
-        canIUseGetUserProfile: true
+        list: res.data.data
       })
-    }
+    })
+    getbanner().then(res => {
+      this.setData({
+        bannerList: res.data.data
+      })
+    })
+    getFree().then(res => {
+      console.log(res.data.data.course_list);
+      this.setData({
+        freeList: res.data.data.course_list,
+      })
+    })   
   },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
